8d961f5c8cba8b33ae0919747031fffc66c33117
“AntoineRacine” <antoine.racine@yahoo.com>
2026-01-19 16:18:49 -0500

    YYYY-01-Jan 19, 2026 16:18:47

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+/* document.addEventListener("DOMContentLoaded", function() {
+  const toggle = document.getElementById("mobile-sidebar-toggle");
+  if (toggle) {
+    toggle.addEventListener("click", function() {
+      document.body.classList.toggle("sidebar-open");
+    });
+  }
+});
